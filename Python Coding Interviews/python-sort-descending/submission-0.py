79f1ed292ec334c32e87ba26cd94730ea6b6b767
Konain Worsi <konainworsi@gmail.com>
2026-08-31 14:27:28 +0530

    Add: python-sort-descending - submission-0

---
+++ Python Coding Interviews/python-sort-descending/submission-0.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    n = len(words)
+    result = words.copy()
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if result[j] < result[j + 1]:
+                result[j], result[j + 1] = result[j + 1], result[j]
+
+    return result
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    n = len(numbers)
+    result = numbers.copy()
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if result[j] < result[j + 1]:
+                result[j], result[j + 1] = result[j + 1], result[j]
+
+    return result
+
+def sort_decimals(numbers: List[float]) -> List[float]:
+    n = len(numbers)
+    result = numbers.copy()
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if result[j] < result[j + 1]:
+                result[j], result[j + 1] = result[j + 1], result[j]
+
+    return result
+
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, 5, 3, 2, 4, 11, 19, 9, 2, 5, 6, 7, 4, 2, 6]))
+
+print(sort_decimals([3.14, 2.82, 6.433, 7.9, 21.555, 21.554]))
